4d73d13f6a556b89cb2997df8f3734e0df1aa9fd
lida Gholamian <ta.gholamian@gmail.com>
2025-10-29 10:46:54 +0330

    create loading component

---
+++ src/app/_components/loading/laoding.tsx
@@ -0,0 +1,27 @@
+import { Size } from "../types/size.type";
+import { LoadingProps } from "./loading.types";
+import classNames from "classnames";
+
+const sizeClasses: Record<Size, string> = {
+  tiny: "loading-xs",
+  small: "loading-sm",
+  normal: "loading-md",
+  large: "loading-lg",
+};
+
+export const Loading: React.FC<LoadingProps> = ({
+  type = "spinner",
+  variant,
+  size = "normal",
+  className,
+}: LoadingProps) => {
+  const classes = classNames(
+    "loading",
+    className,
+    { [`${sizeClasses[size]}`]: size },
+    { [`loading-${variant}`]: variant },
+    { [`loading-${type}`]: type }
+  );
+
+  return <span className={classes}></span>;
+};
