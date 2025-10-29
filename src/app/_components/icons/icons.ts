d2384787453eb55d94905b95c29939eb8a9417c6
lida Gholamian <ta.gholamian@gmail.com>
2025-10-29 19:16:18 +0330

    icons component

---
+++ src/app/_components/icons/icons.ts
@@ -0,0 +1,15 @@
+/* GENERATED FILE */
+
+export { default as IconArrowLeftFill } from './src/arrow-left-fill';
+export { default as IconArrowLeft } from './src/arrow-left';
+export { default as IconBasket } from './src/basket';
+export { default as IconCalendar } from './src/calendar';
+export { default as IconCheck } from './src/check';
+export { default as IconClock } from './src/clock';
+export { default as IconClose } from './src/close';
+export { default as IconComment } from './src/comment';
+export { default as IconDoc } from './src/doc';
+export { default as IconDot } from './src/dot';
+export { default as IconDownload } from './src/download';
+export { default as IconEnrollment } from './src/enrollment';
+export { default as IconEye } from './src/eye';
