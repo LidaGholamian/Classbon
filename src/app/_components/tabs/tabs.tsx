2eca420be629ab8b8302e8cd969ba2c91aaa9f79
lida Gholamian <ta.gholamian@gmail.com>
2026-06-16 14:57:32 +0330

    add alert component

--- src/app/_components/tabs/tabs.tsx
+++ src/app/_components/tabs/tabs.tsx
@@ -1,6 +1,8 @@
 "use client";
-import React, { useState } from "react";
+
+import { useState } from "react";
 import { TabsProps } from "./tabs.types";
+import { Accordion } from "../accordion";
 
 export const Tabs: React.FC<TabsProps> = ({ tabs }) => {
   const [activeTab, setActiveTab] = useState<number>(0);
@@ -28,7 +30,15 @@ export const Tabs: React.FC<TabsProps> = ({ tabs }) => {
           key={`tab-content-${index}`}
           style={{ display: activeTab !== index ? "none" : "block" }}
         >
-          {typeof tab.content === "string" ? (
+          {tab.type === "faq" && tab.faqData ? (
+            tab.faqData.length > 0 ? (
+              <Accordion data={tab.faqData} />
+            ) : (
+              <div className="text-center py-8 text-base-content/60">
+                هیچ سوال متداولی موجود نیست.
+              </div>
+            )
+          ) : typeof tab.content === "string" ? (
             <div
               dangerouslySetInnerHTML={{
                 __html: tab.content as TrustedHTML,
@@ -42,58 +52,3 @@ export const Tabs: React.FC<TabsProps> = ({ tabs }) => {
     </div>
   );
 };
-
-// "use client";
-
-// import { useState } from "react";
-// import { TabsProps } from "./tabs.types";
-// import { Accordion } from "../accordion";
-
-// export const Tabs: React.FC<TabsProps> = ({ tabs }) => {
-//   const [activeTab, setActiveTab] = useState<number>(0);
-
-//   const handleTabClick = (index: number) => {
-//     setActiveTab(index);
-//   };
-
-//   return (
-//     <div className="tabs">
-//       <div className="tab-labels">
-//         {tabs.map((tab, index) => (
-//           <a
-//             key={`tab-${index}`}
-//             className={`tab-label ${index === activeTab ? "tab-active" : ""}`}
-//             onClick={() => handleTabClick(index)}
-//           >
-//             {tab.label}
-//           </a>
-//         ))}
-//       </div>
-//       {tabs.map((tab, index) => (
-//         <div
-//           className="tab-content"
-//           key={`tab-content-${index}`}
-//           style={{ display: activeTab !== index ? "none" : "block" }}
-//         >
-//           {tab.type === "faq" && tab.faqData ? (
-//             tab.faqData.length > 0 ? (
-//               <Accordion data={tab.faqData} />
-//             ) : (
-//               <div className="text-center py-8 text-base-content/60">
-//                 هیچ سوال متداولی موجود نیست.
-//               </div>
-//             )
-//           ) : typeof tab.content === "string" ? (
-//             <div
-//               dangerouslySetInnerHTML={{
-//                 __html: tab.content as TrustedHTML,
-//               }}
-//             />
-//           ) : (
-//             tab.content
-//           )}
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
