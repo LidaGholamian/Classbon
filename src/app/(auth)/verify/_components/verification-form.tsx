b2b4301f09b4b3f720260b1c7e9235141594d8df
lida Gholamian <ta.gholamian@gmail.com>
2026-06-22 22:19:20 +0330

    add auth code

--- src/app/(auth)/verify/_components/verification-form.tsx
+++ src/app/(auth)/verify/_components/verification-form.tsx
@@ -1,17 +1,25 @@
+"use client";
+
 import AuthCode from "@/app/_components/auth-code/auth-code";
+import { AuthCodeRef } from "@/app/_components/auth-code/auth-code.types";
 import { Button } from "@/app/_components/button/button";
 import Link from "next/link";
+import { useRef } from "react";
 
 const VerificationForm = () => {
+  const authCodeRef = useRef<AuthCodeRef>(null);
   return (
     <>
       <h5 className="text-2xl">کد تایید</h5>
       <p className="mt-2">دنیای شگفت انگیز برنامه نویسی در انتظار شماست!</p>
       <form className="flex flex-col gap-6 mt-10 flex-1">
-        <AuthCode className="mt-10" onChange={(value) => {
-                        console.log(value);
-                    }
-        } />
+        <AuthCode
+          className="mt-10"
+          ref={authCodeRef}
+          onChange={(value) => {
+            console.log(value);
+          }}
+        />
         Timer
         <Button isLink={true}>ارسال مجدد کد تایید</Button>
         <Button type="submit" variant="primary">
